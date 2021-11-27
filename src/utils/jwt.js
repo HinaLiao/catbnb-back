09b20eae5f06d4066cb4fb0ac5b32dbc8232bb4b
Hina Liao <hinah.zh@gmail.com>
2021-11-27 16:12:12 -0300

    created jwt

---
+++ src/utils/jwt.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export const generateLoginToken = (payload) => (
+  jwt.sign(
+    payload,
+    process.env.TOKEN_LOGIN_SK,
+    { expiresIn: process.env.LOGIN_TOKEN_EXPIRATION },
+  )
+);
+
+export const verifyLoginToken = (token) => (
+  jwt.verify(token, process.env.TOKEN_LOGIN_SK)
+);

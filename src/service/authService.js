430bd3205be60c7fcc045584cfc8db098b36aa8f
Hina Liao <hinah.zh@gmail.com>
2021-11-30 16:10:41 -0300

    Implemented login service

---
+++ src/service/authService.js
@@ -0,0 +1,61 @@
+import { generateHash, compareHash } from '../utils/bcrypt';
+import { generateLoginToken } from '../utils/jwt';
+
+import UserExistsException from '../exceptions/UserExistsException';
+import InvalidCredentialsException from '../exceptions/InvalidCredentialsException';
+
+class AuthService {
+  constructor(authRepository) {
+    this.authRepository = authRepository;
+  }
+
+  async register(body) {
+    await body.validate();
+
+    const foundUser = await this.authRepository.findUserByEmail(body.email);
+
+    if (foundUser) {
+      throw new UserExistsException();
+    }
+
+    const encryptedPassword = generateHash(body.password, 10);
+
+    const newUser = { ...body, password: encryptedPassword };
+
+    const savedUser = await this.authRepository.saveNewUser(newUser);
+
+    return {
+      id: savedUser._id,
+      name: savedUser.name,
+      email: savedUser.email,
+    };
+  }
+
+  async login(body) {
+    await body.validate();
+
+    const foundUser = await this.authRepository.findUserByEmail(body.email);
+
+    if (!foundUser) {
+      throw new InvalidCredentialsException();
+    }
+
+    const passwordMatches = compareHash(body.password, foundUser.password);
+
+    if (!passwordMatches) {
+      throw new InvalidCredentialsException();
+    }
+
+    const token = generateLoginToken({
+      id: foundUser._id,
+      role: foundUser.role,
+    });
+
+    return {
+      token,
+      role: foundUser.role,
+    };
+  }
+}
+
+export default AuthService;

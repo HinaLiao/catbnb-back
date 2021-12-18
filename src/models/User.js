09f659ad8bafba6a1509a5000edf2bb52324bff6
Hina Liao <hinah.zh@gmail.com>
2021-12-18 13:25:27 -0300

    Add required false to user unavailable days

--- src/models/User.js
+++ src/models/User.js
@@ -11,21 +11,27 @@ const userSchema = new Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['Host', 'Customer'], default: 'Customer' },
-    unavailableWeekDays: { type: Number, enum: [0, 1, 2, 3, 4, 5, 6] },
-    about: { type: String, minleght: 50, maxlength: 350 },
-    skills: [{
-      type: String,
-      enum: [
-        'Primeiros socorros veterinário',
-        'Aplica medicação oral',
-        'Aplica injetáveis',
-        'Aceita gatos idosos',
-        'Tem experiência com idosos',
-        'Supervisão 24h',
-        'Transporte de emergência',
-      ],
+    unavailableWeekDays: {
+      type: Number,
+      enum: [0, 1, 2, 3, 4, 5, 6],
       required: false,
-    }],
+    },
+    about: { type: String, minleght: 50, maxlength: 350 },
+    skills: [
+      {
+        type: String,
+        enum: [
+          'Primeiros socorros veterinário',
+          'Aplica medicação oral',
+          'Aplica injetáveis',
+          'Aceita gatos idosos',
+          'Tem experiência com idosos',
+          'Supervisão 24h',
+          'Transporte de emergência',
+        ],
+        required: false,
+      },
+    ],
     active: { type: Boolean, default: true },
   },
   {
